87e5a53457c8c5037827c08832246eb631e0a3ff
Bhavesh Goswami <s572836@nwmissouri.edu>
2024-10-30 00:08:28 -0500

    update the computational.js

--- routes/computation.js
+++ routes/computation.js
@@ -4,21 +4,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   let x = req.query.x ? parseFloat(req.query.x) : Math.random() * 100;
 
-  let y;
-  let fnName;
-  if (x % 3 === 0) {
-    y = Math.hypot(x);
-    fnName = "Math.hypot";
-  } else if (x % 3 === 1) {
+  let hypotResult = Math.hypot(x);
+  let ceilResult = Math.ceil(x);
+  let clz32Result = Math.clz32(x);
 
-    y = Math.ceil(x);
-    fnName = "Math.ceil";
-  } else {
-    y = Math.clz32(x);
-    fnName = "Math.clz32";
-  }
+  let response = `
+    Math.hypot applied to ${x.toFixed(2)} is ${hypotResult.toFixed(2)}<br>
+    Math.ceil applied to ${x.toFixed(2)} is ${ceilResult}<br>
+    Math.clz32 applied to ${x.toFixed(2)} is ${clz32Result}
+  `;
 
-  res.send(`${fnName} applied to ${x.toFixed(2)} is ${y}`);
+  res.send(response);
 });
 
 module.exports = router;
