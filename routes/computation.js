76106c3cc7b45eb24a42cc7b92c7cb7a66ac5b00
Bhavesh Goswami <s572836@nwmissouri.edu>
2024-10-30 00:03:37 -0500

    Update the computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -2,13 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-
   let x = req.query.x ? parseFloat(req.query.x) : Math.random() * 100;
-  
-  
-  let y = Math.hypot(x);
 
-  res.send(`Math.hypot applied to ${x.toFixed(2)} is ${y.toFixed(2)}`);
+  let y;
+  let fnName;
+  if (x % 3 === 0) {
+    y = Math.hypot(x);
+    fnName = "Math.hypot";
+  } else if (x % 3 === 1) {
+
+    y = Math.ceil(x);
+    fnName = "Math.ceil";
+  } else {
+    y = Math.clz32(x);
+    fnName = "Math.clz32";
+  }
+
+  res.send(`${fnName} applied to ${x.toFixed(2)} is ${y}`);
 });
 
 module.exports = router;
